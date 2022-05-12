6874fef43d45a36b9701acf43d07bd052bd77a3f
Graeme Winter <graeme.winter@gmail.com>
2022-05-12 17:56:14 +0100

    Basic PIO driven IRQ

--- picoripheral.c
+++ picoripheral.c
@@ -1,23 +1,59 @@
 #include <stdio.h>
+
 #include "pico/stdlib.h"
-#include "hardware/i2c.h"
+#include "hardware/pio.h"
+#include "hardware/gpio.h"
+#include "hardware/clocks.h"
+#include "picoripheral.pio.h"
 
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
-#define I2C_FREQ 400000
+#define TRIGGER 16
+#define COUNTER 17
+
+void picoripheral_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq, bool enable);
+
+volatile uint32_t counter;
+
+void __not_in_flash_func(callback)(uint gpio, uint32_t event) {
+  if (gpio == COUNTER) {
+    if (event == GPIO_IRQ_EDGE_FALL) {
+      counter ++;
+    }
+  }
+  if (gpio == TRIGGER) {
+    if (event == GPIO_IRQ_EDGE_RISE) {
+      pio_sm_set_enabled(pio1, 0, true);
+      counter = 0;
+    } else {
+      pio_sm_set_enabled(pio1, 0, false);
+      printf("%d\n", counter);
+    }
+  } 
+}
 
 
-int main()
-{
-    stdio_init_all();
+int main() {
+    setup_default_uart();
 
-    i2c_init(I2C_PORT, I2C_FREQ);
-    
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
+    // set up the IRQ
+    uint32_t irq_mask = GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL;
+    gpio_set_irq_enabled_with_callback(TRIGGER, irq_mask, true, &callback);
+    gpio_set_irq_enabled(COUNTER, irq_mask, true);
 
-    return 0;
+    // bulk clock pio
+    uint off0 = pio_add_program(pio0, &picoripheral_program);
+    picoripheral_pin_forever(pio0, 0, off0, 15, 10, true);
+    picoripheral_pin_forever(pio0, 1, off0, 25, 10, true);
+
+    // fast clock pio
+    uint off1 = pio_add_program(pio1, &picoripheral_program);    
+    picoripheral_pin_forever(pio1, 0, off1, 14, 20000, false);
+
+    while (true);
+}
+
+void picoripheral_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq, bool enable) {
+    picoripheral_program_init(pio, sm, offset, pin);
+    pio_sm_set_enabled(pio, sm, enable);
+
+    pio->txf[sm] = clock_get_hz(clk_sys) / (2 * freq);
 }
