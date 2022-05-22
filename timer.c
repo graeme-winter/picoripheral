72a8a6be215415d444f0bccae75f23100c1cb152
Graeme Winter <graeme.winter@gmail.com>
2022-05-22 16:32:51 +0100

    Add timer delay (#1)
    * Clean up timer
    * Added delay PIO program
    * C API - single function

--- timer.c
+++ timer.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#include "delay.pio.h"
 #include "hardware/clocks.h"
 #include "hardware/gpio.h"
 #include "hardware/pio.h"
@@ -11,7 +12,8 @@
 #define CLOCK0 16
 #define CLOCK1 17
 
-void timer_no_delay(PIO pio, uint sm, uint pin, uint32_t high, uint32_t low, bool enable);
+void timer(PIO pio, uint sm, uint pin, uint32_t delay, uint32_t high,
+           uint32_t low, bool enable);
 
 volatile uint32_t counter;
 volatile uint64_t t0, t1;
@@ -39,33 +41,49 @@ int main() {
   setup_default_uart();
 
   uint32_t freq = clock_get_hz(clk_sys);
-  
+
   // set up the IRQ
   uint32_t irq_mask = GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL;
   gpio_set_irq_enabled_with_callback(TRIGGER, irq_mask, true, &callback);
   gpio_set_irq_enabled(COUNTER, irq_mask, true);
 
   // bulk clock pio
-  timer_no_delay(pio0, 0, CLOCK1, freq / 2, freq / 2, true);
-  timer_no_delay(pio0, 1, 25, freq / 2, freq / 2, true);
+  timer(pio0, 0, CLOCK1, 0, freq / 2, freq / 2, true);
+  timer(pio0, 1, 25, 3 * freq, freq / 2, freq / 2, true);
 
-  // fast clock pio
-  timer_no_delay(pio1, 0, CLOCK0, freq / 20000, 9 * freq / 20000, false);
+  // fast clock - enabled by interrupt
+  timer(pio1, 0, CLOCK0, 0, freq / 20000, 9 * freq / 20000, false);
 
   while (true) {
     tight_loop_contents();
   }
 }
 
-// no-delay timer program
-void timer_no_delay(PIO pio, uint sm, uint pin, uint32_t high, uint32_t low, bool enable) {
-  // load program, init, push to registers
-  uint offset = pio_add_program(pio, &timer_program);
-  timer_program_init(pio, sm, offset, pin);
+// with-delay timer program
+void timer(PIO pio, uint sm, uint pin, uint32_t delay, uint32_t high,
+           uint32_t low, bool enable) {
+  // if delay, load one program else other
+  if (delay == 0) {
+    uint offset = pio_add_program(pio, &timer_program);
+    timer_program_init(pio, sm, offset, pin);
+    // intrinsic delays - I _think_ the delay on 1st cycle is 2 not 3
+    delay -= 2;
+  } else {
+    uint offset = pio_add_program(pio, &delay_program);
+    delay_program_init(pio, sm, offset, pin);
+  }
 
-  // intrinsic delays
+  // intrinsic delays - these are certainly 3 cycles
   high -= 3;
   low -= 3;
+
+  if (delay != 0) {
+    // load delay into OSR then copy to Y if we are running delay program
+    // otherwise Y register unused
+    pio->txf[sm] = delay;
+    pio_sm_exec(pio, sm, pio_encode_pull(false, false));
+    pio_sm_exec(pio, sm, pio_encode_out(pio_y, 32));
+  }
 
   // load low into OSR then copy to ISR
   pio->txf[sm] = low;
