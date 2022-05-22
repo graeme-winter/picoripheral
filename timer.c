b365f9eecf3bbc64f06265acc09bdffab70c7243
Graeme Winter <graeme.winter@gmail.com>
2022-05-22 06:08:19 +0100

    External wiring important; clocks

--- timer.c
+++ timer.c
@@ -6,14 +6,16 @@
 #include "pico/stdlib.h"
 #include "timer.pio.h"
 
-#define TRIGGER 16
-#define COUNTER 17
-#define ADC0 26
+#define TRIGGER 14
+#define COUNTER 15
+#define CLOCK0 16
+#define CLOCK1 17
 
 void picoripheral_pin_forever(PIO pio, uint sm, uint offset, uint pin,
                               uint freq, bool enable);
 
 volatile uint32_t counter;
+volatile uint64_t t0, t1;
 
 void __not_in_flash_func(callback)(uint gpio, uint32_t event) {
   if (gpio == COUNTER) {
@@ -24,10 +26,12 @@ void __not_in_flash_func(callback)(uint gpio, uint32_t event) {
   if (gpio == TRIGGER) {
     if (event == GPIO_IRQ_EDGE_RISE) {
       pio_sm_set_enabled(pio1, 0, true);
+      t0 = time_us_64();
       counter = 0;
     } else {
       pio_sm_set_enabled(pio1, 0, false);
-      printf("%d\n", counter);
+      t1 = time_us_64();
+      printf("%d %ld\n", counter, t1 - t0);
     }
   }
 }
@@ -42,12 +46,12 @@ int main() {
 
   // bulk clock pio
   uint off0 = pio_add_program(pio0, &timer_program);
-  picoripheral_pin_forever(pio0, 0, off0, TRIGGER, 1, true);
+  picoripheral_pin_forever(pio0, 0, off0, CLOCK1, 1, true);
   picoripheral_pin_forever(pio0, 1, off0, 25, 1, true);
 
   // fast clock pio
   uint off1 = pio_add_program(pio1, &timer_program);
-  picoripheral_pin_forever(pio1, 0, off1, COUNTER, 20000, false);
+  picoripheral_pin_forever(pio1, 0, off1, CLOCK0, 10000, false);
 
   while (true) {
     tight_loop_contents();
