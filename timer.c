7d69fde56e830e43d492a6d23b948fd40d15715c
Graeme Winter <graeme.winter@gmail.com>
2022-05-23 18:03:37 +0100

    Check delay count (#2)
    * First cut at clock delay debug

--- timer.c
+++ timer.c
@@ -23,30 +23,16 @@ volatile uint64_t t0, t1;
 void __not_in_flash_func(callback)(uint gpio, uint32_t event) {
   if (gpio == COUNTER) {
     if (event == GPIO_IRQ_EDGE_FALL) {
-      counter++;
+      t0 = time_us_64();
     }
   } else if (gpio == TRIGGER) {
-    if (event == GPIO_IRQ_EDGE_RISE) {
-      pio_sm_set_enabled(pio1, 0, true);
-      t0 = time_us_64();
-      counter = 0;
-    } else {
-      pio_sm_set_enabled(pio1, 0, false);
+    if (event == GPIO_IRQ_EDGE_FALL) {
       t1 = time_us_64();
       printf("%d %ld\n", counter, t1 - t0);
     }
   } else if (gpio == EXTERNAL) {
     if (event == GPIO_IRQ_EDGE_RISE) {
       pio_enable_sm_mask_in_sync(pio1, 0b11);
-      t0 = time_us_64();
-      counter = 0;
-    } else {
-      pio_sm_set_enabled(pio1, 0, false);
-      pio_sm_set_enabled(pio1, 1, false);
-      pio_sm_exec(pio1, 0, pio_encode_set(pio_pins, 0));
-      pio_sm_exec(pio1, 1, pio_encode_set(pio_pins, 0));
-      t1 = time_us_64();
-      printf("%d %ld\n", counter, t1 - t0);
     }
   }
 }
@@ -64,12 +50,9 @@ int main() {
   gpio_set_irq_enabled(COUNTER, irq_mask, true);
   gpio_set_irq_enabled(EXTERNAL, irq_mask, true);
 
-  // internal trigger clock - disabled
-  timer(pio0, 0, CLOCK1, 0, freq / 2, freq / 2, false);
-
   // fast clocks - enabled by interrupt
-  timer(pio1, 0, CLOCK0, 0, freq / 200, freq / 200, false);
-  timer(pio1, 1, 25, 0, freq / 2000, freq / 2000, false);
+  timer(pio1, 0, CLOCK0, 0, 100, 9900, false);
+  timer(pio1, 1, CLOCK1, 5, 100, 9900, false);
 
   while (true) {
     tight_loop_contents();
@@ -88,6 +71,7 @@ void timer(PIO pio, uint sm, uint pin, uint32_t delay, uint32_t high,
   } else {
     uint offset = pio_add_program(pio, &delay_program);
     delay_program_init(pio, sm, offset, pin);
+    delay -= 2;
   }
 
   // intrinsic delays - these are certainly 3 cycles
